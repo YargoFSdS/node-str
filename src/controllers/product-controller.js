5a4cf18b70c7ad3319105482896534cc83bc2049
Yargo Fernando <yargo.fernando@dbseller.com.br>
2018-11-14 17:16:23 -0200

    Novos Métodos criados, e criado validador

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,8 +4,18 @@ const mongoose = require('mongoose');
  //const Product  = mongoose.model('Product'); 
 var Product = require('./../models/product.js');
 
+var ValidationContract = require('../validators/fluent-validator');
+
 exports.post = (req,res,next) => {
-    //res.status(200).send(req.body); // 200 ok 201 create 400 bad request 401 nao autenticado 403 acesso negado 500 internal server error
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title,3,'O titulo deve ter pelo menos 3 caracters');
+    contract.hasMinLen(req.body.slug,3,'O titulo deve ter pelo menos 3 caracters');
+    contract.hasMinLen(req.body.description,3,'O titulo deve ter pelo menos 3 caracters');
+
+    if(!contract.isValid()){
+        res.status(400).send(contract.errors()).end();
+    }
+
     var product = new Product(req.body);
     // product.title = req.body.title;   
     product.save((err) => {
@@ -20,7 +30,48 @@ exports.post = (req,res,next) => {
 
 exports.get = (req,res,next) => {
     Product
-        .find({})
+        .find({ //.find({titie:'xpto', descri: 'computador'})
+            active: true
+        })
+        .then(data =>{ 
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(401).send(e);
+        });     
+}
+
+exports.getBySlug = (req,res,next) => {
+    Product
+        .findOne({ //.find({titie:'xpto', descri: 'computador'})
+            slug: req.params.slug,
+            active: true
+        },'title description price slug')
+        .then(data =>{ 
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(401).send(e);
+        });     
+}
+
+
+
+exports.getById = (req,res,next) => {
+    Product
+        .findById({ //.find({titie:'xpto', descri: 'computador'})
+            _id: req.params.id
+        })
+        .then(data =>{ 
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(401).send(e);
+        });     
+}
+
+exports.getByTag = (req,res,next) => {
+    Product
+        .find({ //.find({titie:'xpto', descri: 'computador'})
+            tags: req.params.tag
+        })
         .then(data =>{ 
             res.status(200).send(data);
         }).catch(e => {
@@ -29,15 +80,38 @@ exports.get = (req,res,next) => {
 }
 
 exports.put = (req,res,next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id, 
-        item: req.body 
-     }); // 200 ok 201 create 400 bad request 401 nao autenticado 403 acesso negado 500 internal server error
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: { 
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                slug: req.body.slug
+            }
+        }).then(x => {
+            res.status(200).send({
+                message: 'Produto atualizado com Sucesso!'
+            });
+
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar Produto!'
+            });
+        });
 };
 
 exports.delete = (req,res,next) => {
-    const id = req.params.id;
-    res.status(200).send(req.body); // 200 ok 201 create 400 bad request 401 nao autenticado 403 acesso negado 500 internal server error
+    Product
+        .findOneAndRemove(req.body.id, {            
+        }).then(x => {
+            res.status(200).send({
+                message: 'Produto ecluido com Sucesso!'
+            });
+
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao excluir Produto!'
+            });
+        });
 };
 // dont forget mlab.com
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,6 +5,9 @@ const controller = require('../controllers/product-controller.js');
 
 
 router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tag/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
